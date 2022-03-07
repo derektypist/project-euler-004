13942a7829c20180a1dc6885cdd152f7fd214d98
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-03-07 10:32:18 +0000

    Added largestPalindromeProduct function at script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+/*
+    Function to Return the largest palindrome made from the product of
+    two n-digit numbers.
+    largestPalindromeProduct(2) returns 9009
+
+*/
+function largestPalindromeProduct(n) {
+    let max = +[...Array(n)].reduce((a,c) => (a+=9),"");
+    let min = (max+1)/10;
+    let res = [];
+    for (let i=max;i>=min;i--) {
+        for (let j=max;j>=min;j--) {
+            let num = i*j;
+            let numReverse = [...String(num)].reverse().join('');
+            if (num == numReverse) {
+                res.push(num);
+                break;
+            }
+        }
+    }
+    return Math.max(...res);
+
+}
